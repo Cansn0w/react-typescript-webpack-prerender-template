b9ddc2629516ef7ffab79ace01b518d9a397b241
Cansn0w <c@cansnow.net>
2022-07-17 18:30:16 +1000

    upgrate to react 18

--- src/hydrate.ts
+++ src/hydrate.ts
@@ -1,8 +1,8 @@
-import * as ReactDom from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 import { createApp } from "app/create";
 import "./styles.css";
 
 window.addEventListener("DOMContentLoaded", () => {
   const bootstrap = (window as any)["bootstrap"] as { content: string };
-  ReactDom.hydrate(createApp(bootstrap), document.getElementById("root"));
+  hydrateRoot(document.getElementById("root")!, createApp(bootstrap));
 });
